cd776855b97f03178f3fcea00390f4503c6dadb2
Paul Pan <panjunhong05@gmail.com>
2022-05-21 10:47:14 -0700

    Further modifications.

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,7 +6,7 @@ import styles from './index.module.scss'
 const Header: NextComponentType = () => {
   return (
     <>
-      <Image src={ProfilePicture} alt='Profile Picture' className={styles.profileImage} />
+      <Image src={ProfilePicture} alt='Profile Picture' className={styles['profile-image']} />
       <h1>Hi everyone! I&apos;m Paul.</h1>
       <h2>Welcome to my homepage!</h2>
     </>
