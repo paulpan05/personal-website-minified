66581856c0c7ec45211a7a4551004b49502f09eb
Paul Pan <panjunhong05@gmail.com>
2022-05-21 03:54:55 -0700

    Modified start page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
+import Head from 'next/head'
 import ProfilePicture from '../public/profile_picture.jpg'
 
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Paul&apos;s Main Website</title>
+        <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32' />
+        <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+        <link rel='manifest' href='/manifest.json' />
+      </Head>
       <h1>Hi everyone! I&apos;m Paul.</h1>
       <h2>Welcome to my homepage!</h2>
       <Image src={ProfilePicture} alt='Profile Picture' />
