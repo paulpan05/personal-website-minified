c2e2950bbdcc35c463ae6036bc335aa6f3ac827d
Paul Pan <panjunhong05@gmail.com>
2023-01-07 14:50:59 -0500

    Add png extension to favicon.

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,8 @@ const Home: NextPage = () => {
       <Head>
         <title>Paul&apos;s Main Website</title>
         <link rel='icon' type='image/x-icon' href='/favicon.ico' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
         <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
         <link rel='manifest' href='/manifest.json' />
       </Head>
