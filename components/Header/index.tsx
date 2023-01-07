3d6ff63cde275c9f83783d82f2c5cbb744082449
Paul Pan <panjunhong05@gmail.com>
2023-01-07 16:02:24 -0500

    Added header implementation.

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,11 +4,11 @@ import styles from './index.module.scss'
 
 const Header: NextComponentType = () => {
   return (
-    <>
-      <Image src='/image/profile_picture.jpg' alt='Profile Picture' className={styles['profile-image']} width={500} height={500} />
+    <div className={styles['header']}>
+      <Image src='/image/profile_picture.jpg' alt='Profile Picture' className={styles['profile-image']}  width={250} height={250}/>
       <h1>Hi everyone! I&apos;m Paul.</h1>
       <h2>Welcome to my homepage!</h2>
-    </>
+    </div>
   );
 }
 
