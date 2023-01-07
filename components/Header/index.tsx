520e5b385d3e11d742f3be3a6715c872f372f8a0
Paul Pan <panjunhong05@gmail.com>
2023-01-06 23:46:52 -0500

    Moved image location and add sharp dependency.

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,12 +1,11 @@
 import type { NextComponentType } from 'next'
 import Image from 'next/image'
-import ProfilePicture from '../../assets/img/profile_picture.jpg'
 import styles from './index.module.scss'
 
 const Header: NextComponentType = () => {
   return (
     <>
-      <Image src={ProfilePicture} alt='Profile Picture' className={styles['profile-image']} />
+      <Image src='/image/profile_picture.jpg' alt='Profile Picture' className={styles['profile-image']} width={500} height={500} />
       <h1>Hi everyone! I&apos;m Paul.</h1>
       <h2>Welcome to my homepage!</h2>
     </>
