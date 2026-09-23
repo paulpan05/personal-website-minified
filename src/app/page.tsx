1bf48e6bd8256436ee5e00995b47a3b0e17bfaa4
Paul Pan <personal@panpaul.com>
2026-09-22 23:53:54 -0400

    Remove homepage Writing section; Writing lives behind SiteNav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Header from '@/components/Header/Header'
 import About from '@/components/About/About'
 import Experience from '@/components/Experience/Experience'
 import HighlightedProjects from '@/components/HighlightedProjects/HighlightedProjects'
-import Writing from '@/components/Writing/Writing'
 import MoreAboutMe from '@/components/MoreAboutMe/MoreAboutMe'
 
 export default function Home() {
@@ -13,7 +12,6 @@ export default function Home() {
         <About />
         <Experience />
         <HighlightedProjects />
-        <Writing />
         <MoreAboutMe />
       </main>
     </>
